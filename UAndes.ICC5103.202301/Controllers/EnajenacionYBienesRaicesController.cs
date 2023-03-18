2c82e544a25cc9e1a04df7fd5d2beadb8c128607
Matías Bustos <matias.bustosa@gmail.com>
2023-03-18 19:00:08 -0300

    some forms added and controller Enajenacion y bienes raices

---
+++ UAndes.ICC5103.202301/Controllers/EnajenacionYBienesRaicesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UAndes.ICC5103._202301.Controllers
+{
+    public class EnajenacionYBienesRaicesController : Controller
+    {
+        // GET: EnajenacionYBienesRaices
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
